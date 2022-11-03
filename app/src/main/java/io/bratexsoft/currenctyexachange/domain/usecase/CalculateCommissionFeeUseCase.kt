46faab08741812a7c4d6910163367d120bec84e7
Mateusz Bratkowski <mateusz.bratkowski@bamtechmedia.com>
2022-11-03 19:45:01 +0100

    Update test case

--- app/src/main/java/io/bratexsoft/currenctyexachange/domain/usecase/CalculateCommissionFeeUseCase.kt
+++ app/src/main/java/io/bratexsoft/currenctyexachange/domain/usecase/CalculateCommissionFeeUseCase.kt
@@ -3,6 +3,7 @@ package io.bratexsoft.currenctyexachange.domain.usecase
 import io.bratexsoft.currenctyexachange.DateProvider
 import io.bratexsoft.currenctyexachange.local.room.datasource.counter.ExchangeCounterLocalDataSource
 import java.math.BigDecimal
+import java.math.RoundingMode
 import javax.inject.Inject
 
 class CalculateCommissionFeeUseCase @Inject constructor(
@@ -13,11 +14,14 @@ class CalculateCommissionFeeUseCase @Inject constructor(
     suspend operator fun invoke(
         value: Double,
     ): BigDecimal {
-        val exchangeCount = exchangeCounterLocalDataSource.getCounterState(dateProvider.provideCurrentDateInNormalDate().time)
+        val exchangeCount =
+            exchangeCounterLocalDataSource.getCounterState(dateProvider.provideCurrentDateInNormalDate().time)
         return when {
             exchangeCount < 5 -> BigDecimal(0)
             exchangeCount in 5..14 -> BigDecimal(value).multiply(BigDecimal(0.007))
-            else -> BigDecimal(value).multiply(BigDecimal(0.012)).plus(BigDecimal(0.003))
+                .setScale(2, RoundingMode.HALF_UP)
+            else -> BigDecimal(value).multiply(BigDecimal(0.012)).plus(BigDecimal(0.3))
+                .setScale(2, RoundingMode.HALF_UP)
         }
     }
 }
